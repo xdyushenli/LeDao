82f9c1db7dab9f32b5ffceddf9d876298b9fa400
bethelight <1598204060@qq.com>
2018-11-21 01:31:32 +0800

    update

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,20 @@
-import { wxRequest } from '../utils/wxRequest'
+import { wxRequest } from './wxRequest.js'
 
-//  获取视频教程接口
-const getVideo = (params) => wxRequest(params, '/singlePoetry')
+// 验证验证码
+const veriCode = (p) => wxRequest(p, '/code', 'POST')
+
+// 登录接口
+const login = (p) => wxRequest(p, '/login', 'POST')
+
+// 获取视频教程接口
+const getVideo = (params) => wxRequest(params, `/courses/${params.courseName}/video`, 'GET')
+
+// 签到  疑问？如果连续点击多次的结果
+const signIn = (params) => wxRequest(params, `/children/${params.id}/sign`, 'PUT')
 
 export default {
-  getVideo
+  veriCode,
+  login,
+  getVideo,
+  signIn
 }
---
+++ src/api/wxRequest.js
@@ -0,0 +1,21 @@
+import wepy from 'wepy'
+
+const baseURL = 'http://39.106.63.214:8888'
+
+// request的HTTP请求封装
+const wxRequest = async(params = {}, url, method) => {
+  // loading() 显示加载
+  let data = params.data || {}
+  let res = await wepy.request({
+    url: baseURL + url,
+    data: data,
+    method: method || 'GET',
+    header: params.header || {'Content-Type': 'application/json'}
+  })
+  // loaded() 加载完成
+  return res
+}
+
+module.exports = {
+  wxRequest
+}
